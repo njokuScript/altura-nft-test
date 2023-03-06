8a476d789f24f21bae9870725e736668b63c269a
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-03-07 01:56:27 +0400

    chore: restructure components

---
+++ src/view/Home.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function App() {
+  return <div className='bg-primary w-screen h-screen'></div>;
+}
+
+export default App;
--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../src/view/App';
+import App from '../src/view/Home';
 
 test('renders learn react link', () => {
   render(<App />);
