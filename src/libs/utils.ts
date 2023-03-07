7480124f1039b3de52ef8ae9e4c811c6c792c9e3
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-03-08 02:53:44 +0400

    chore: implement abstracted loader

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -5,3 +5,21 @@ export const truncateAddress = (value: string, strLen = 8) => {
 
   return `${value.substring(0, 4)}...${value.substring(value.length - 4)}`;
 };
+
+export const attachLoader =
+  (setter: (setterFuc: any) => any) =>
+  (name: string, callback: (param?: any) => Promise<any>) =>
+  (...args: any) => {
+    const setLoadingTo = (state: any) => (prevState: any) => {
+      return {
+        ...prevState,
+        [name]: state,
+      };
+    };
+    setter(setLoadingTo(true));
+    return callback(...args)
+      .catch((err: any) => {
+        return Promise.reject(err);
+      })
+      .finally(() => setter(setLoadingTo(false)));
+  };
